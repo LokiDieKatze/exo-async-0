cdfa3f759db8657d77d7b210d333f6bff32a29bb
LokiDieKatze <sarah.marques.fr@gmail.com>
2021-04-14 10:25:57 +0200

    corrigé

--- my-wget.js
+++ my-wget.js
@@ -5,11 +5,11 @@ const main = async () => {
   try {
     let response = await axios.get('https://fr.wikipedia.org/wiki/Akita_(chien)')
 
+    await fsPromises.writeFile('index.html', response.data)
+
     let size = await fsPromises.stat('index.html')
     console.log(size.size)
-    // ou: console.log(response.headers)
-
-    await fsPromises.writeFile('index.html', response.data)
+    // ou: console.log(response.headers['content-length'])
 
   } catch (e) {
     console.log(e.message)
